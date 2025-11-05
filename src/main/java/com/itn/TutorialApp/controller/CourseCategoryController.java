f9bbf95f985be60649cf7784724aa6b500a3c1d6
ronny7-code <bijay98813@gmail.com>
2025-11-05 12:01:44 +0545

    removing Model

--- src/main/java/com/itn/TutorialApp/controller/CourseCategoryController.java
+++ src/main/java/com/itn/TutorialApp/controller/CourseCategoryController.java
@@ -3,7 +3,6 @@ package com.itn.TutorialApp.controller;
 import com.itn.TutorialApp.entity.CourseCategory;
 import com.itn.TutorialApp.service.CourseCategoryService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,20 +22,15 @@ public class CourseCategoryController {
     @PostMapping("/submitCourseCategory")
     public String getCourseCategory(
             @RequestParam("name") String name,
-            @RequestParam("description") String description,
-            Model model) {
+            @RequestParam("description") String description) {
 
         CourseCategory courseCategory = new CourseCategory();
         courseCategory.setName(name);
         courseCategory.setDescription(description);
 
-        boolean success = courseCategoryService.saveCourseCategory(courseCategory);
+       courseCategoryService.saveCourseCategory(courseCategory);
 
-        if (success) {
-            model.addAttribute("successMsg", "Course Category Added Successfully!");
-        } else {
-            model.addAttribute("errorMsg", "Course Category cannot be Added!");
-        }
+    
 
         return "admin/CourseCategory";
     }
